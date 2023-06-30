88992f5570e03f9bb09cd88f8c81fc3c8fd5b583
Chen <136211619@qq.com>
2023-06-30 20:25:49 +0800

    update

--- 17_Optimization Problem/draft.py
+++ 17_Optimization Problem/draft.py
@@ -1,47 +1,94 @@
-import scipy.optimize as optimize
-
-c = [2, 4]  # 目标函数的系数
-A_ub = [[-1, -1], [-4, -1]]  # 不等式约束的系数矩阵
-b_ub = [-3, -9]  # 不等式约束的右侧常数向量
-bounds = [(0, None), (0, None)]  # 变量的边界条件，表示 x₁ 和 x₂ 都为非负数
-
-result = optimize.linprog(c, A_ub=A_ub, b_ub=b_ub, bounds=bounds)
-
-if result.success:
-    print("最优解:")
-    print("x₁ =", result.x[0])
-    print("x₂ =", result.x[1])
-    print("目标函数的最小值:", result.fun)
-else:
-    print("求解失败:", result.message)
-
-
 import numpy as np
-from scipy.optimize import minimize
+def Interior_Point(c,A,b):
+    # 当输入的c,A,b有缺失值时，输出错误原因，函数执行结束
+    if c.shape[0] != A.shape[1]:
+        print("A和C形状不匹配")
+        return 0
+    if b.shape[0] != A.shape[0]:
+        print("A和b形状不匹配")
+        return 0
+    # 初值的设置
+    x=np.ones((A.shape[1],1)) # 将x的初值设为1
+    v=np.ones((b.shape[0],1))  # 将v的初值设为1
+    lam=np.ones((x.shape[0],1))  # 将lam的初值设为1
+    one=np.ones((x.shape[0],1))
+    mu=1 # 将mu的初值设为1
+    n=A.shape[1]
+    x_=np.diag(x.flatten()) # 将x转换为对角矩阵
+    lam_=np.diag(lam.flatten()) # 将lam转换为对角矩阵
+    # 初始的F，r=F
+    r1=np.matmul(A,x)-b
+    r2=np.matmul(np.matmul(x_,lam_),one)-mu*one
+    r3=np.matmul(A.T,v)+c-lam
+    r=np.vstack((r1,r2,r3))
+    F=r
+    # 求得r1、r2、r3的初始范数
+    n1=np.linalg.norm(r1)
+    n2=np.linalg.norm(r2)
+    n3=np.linalg.norm(r3)
+    # nablaF中零矩阵和单位阵的设置
+    zero11=np.zeros((A.shape[0],x.shape[0]))
+    zero12=np.zeros((A.shape[0],A.shape[0]))
+    zero22=np.zeros((x.shape[0],A.shape[0]))
+    zero33=np.zeros((A.shape[1],A.shape[1]))
+    one31=np.eye(A.shape[1])
+    tol=1e-8 # 设置最优条件的容忍度
+    t=1
+    alpha = 0.5
+    while max(n1,n2,n3)>tol:
+        # F的Jacobian矩阵
+        nablaF = np.vstack((np.hstack((zero11, zero12, A))
+                            , np.hstack((x_, zero22, lam_))
+                            , np.hstack((-one31, A.T, zero33))))
+        # F+nablaF@delta=0,解方程nablaF@delta=-r
+        delta = np.linalg.solve(nablaF, -r)  # 解方程，求出delta的值
+        delta_lam = delta[0:lam.shape[0], :]
+        delta_v = delta[lam.shape[0]:lam.shape[0] + v.shape[0], :]
+        delta_x = delta[lam.shape[0] + v.shape[0]:, :]
+        # 更新lam、v、x、mu
+        alpha=Alpha(c,A,b,lam,v,x,alpha,delta_lam,delta_v,delta_x)
+        lam=lam+alpha*delta_lam
+        v=v+alpha*delta_v
+        x=x+alpha*delta_x
+        x_ = np.diag(x.flatten())  # 将x转换为对角矩阵
+        lam_ = np.diag(lam.flatten())  # 将lam转换为对角矩阵
+        mu=(0.1/n)*np.dot(lam.flatten(),x.flatten()) #更新mu的值
+        # 计算更新后的F
+        r1 = np.matmul(A, x) - b
+        r2 = np.matmul(np.matmul(x_, lam_), one) - mu * one
+        r3 = np.matmul(A.T, v) + c - lam
+        r = np.vstack((r1, r2, r3))
+        F = r
+        # 计算更新后F的范数
+        n1 = np.linalg.norm(r1)
+        n2 = np.linalg.norm(r2)
+        n3 = np.linalg.norm(r3)
+        t=t+1
+        z=(c.T @ x).flatten()[0]
+    print("x的取值",x.flatten())
+    print('最优值为',z)
 
-def objective(x):
-    # 定义目标函数
-    return 2 * x[0]**2 + 4 * x[1]**2 - 4 * x[0] * x[1] - 6 * x[0] - 3 * x[1]
+# 寻找alpha
+def Alpha(c,A,b,lam,v,x,alpha,delta_lam,delta_v,delta_x):
+    alpha_x=[]
+    alpha_lam=[]
+    for i in range(x.shape[0]):
+        if delta_x.flatten()[i]<0:
+            alpha_x.append(x.flatten()[i]/-delta_x.flatten()[i])
+        if delta_lam.flatten()[i]<0:
+            alpha_lam.append(lam.flatten()[i]/-delta_lam.flatten()[i])
+    if len(alpha_x)==0 and len(alpha_lam)==0:
+        return alpha
+    else:
+        alpha_x.append(np.inf)
+        alpha_lam.append(np.inf)
+        alpha_x = np.array(alpha_x)
+        alpha_lam= np.array(alpha_lam)
+        alpha_max = min(np.min(alpha_x), np.min(alpha_lam))
+        alpha_k = min(1,0.99*alpha_max)
+    return alpha_k
 
-def constraint1(x):
-    # 定义第一个约束条件
-    return 3 - x[0] - x[1]
-
-def constraint2(x):
-    # 定义第二个约束条件
-    return 9 - 4 * x[0] - x[1]
-
-# 定义初始点和容差
-x0 = [1, 0]
-tol = 0.001
-
-# 定义约束条件字典
-cons = ({'type': 'ineq', 'fun': constraint1},
-        {'type': 'ineq', 'fun': constraint2})
-
-# 使用拟牛顿法进行优化
-result = minimize(objective, x0, method='SLSQP', tol=tol, constraints=cons)
-
-print("优化结果:")
-print("最小值:", result.fun)
-print("最优解:", result.x)
+c = np.array([-5, -1, 0,0]).reshape(-1, 1)
+A = np.array([[1, 1, 1, 0], [2, 0.5, 0, 1]])
+b = np.array([5, 8]).reshape(-1, 1)
+Interior_Point(c,A,b)
--- 17_Optimization Problem/work_linear.py
+++ 17_Optimization Problem/work_linear.py
@@ -1,88 +1,59 @@
 import numpy as np
-from scipy.optimize import optimize
-import re
+from scipy.optimize import linprog
+import numpy as np
+def interior_point_method(A, b, c, x, ite=1000, delta=0.001):
+    m, n = A.shape
+    D = x * np.eye(n, n)
+    res = np.diag(D)
+    for i in range(ite):
+        res = np.diag(D).copy()
+        t = A.dot(D)
+        p = np.eye(D.shape[1]) - t.T.dot(np.linalg.inv(np.matmul(t, t.T))).dot(t)
+        D += (D.dot(p).dot(D).dot(c.T)) / np.linalg.norm(p.dot(D).dot(c.T))
+        D = np.diag(D) * (np.eye(n, n))
+        resnew = np.diag(D)
+        if (sum(abs(resnew - res))) < delta:
+            break
+    return np.round(res, decimals=3)
 
-input_configs = [
-        {
-            'func': "min=2*x_1^2+4*x_2^2-4*x_1*x_2-6*x_1-3*x_2",
-            'initial_point': (1, 0),
-            'cons': ['3-x_1-x_2>=0', '9-4*x_1-x_2>=0', 'x_1>=0', 'x_2>=0'],
-            # 'cons': ['x_1+x_2-3<=0', '4*x_1+x_2-9<=0', '-x_1<=0', '-x_2<=0'],
-            'tol': 0.001
-        },
-        {
-            'func': "min=3/2*x_1^2+1/2*x_2^2-x_1*x_2-2*x_1",
-            'initial_point': (3, 0),
-            # 'cons': ['x_1>=2', 'x_2>=0'],
-            'cons': ['2-x_1<=0', '-x_2<=0'],
-            'tol': 0.001
-        },
-        {
-            'func': "min=x_1^2+25*x_2^2",
-            'initial_point': (1, 0),
-            'cons': ['x_1+x_2>=1'],
-            'tol': 0.001
-        },
-        {
-            'func': "min=x_1^2+x_2^2-4*x_1-4*x_2+8",
-            'initial_point': (1, 0),
-            'cons': ['x_1+2*x_2<=4'],
-            'tol': 0.001
-        },
-        {
-            'func': "min=x_1^2+2*x_2^2+0.3*x_1*x_2-98*x_1-277*x_2",
-            'initial_point': (0, 1),
-            'cons': ['x_1+x_2<=100', 'x_1-2*x_2<=0', 'x_1>=0', 'x_2>=0'],
-            'tol': 0.001
-        }
-    ]
 
-results = []
-for config in input_configs:
-    # 提取问题参数
-    obj_func = config['func']
-    initial_point = config['initial_point']
-    constraints = config['cons']
-    tolerance = config['tol']
 
-    # 提取目标函数的系数及常数项
-    c_idx = max(obj_func.find('x_1'), obj_func.find('x_2'))
-    c_str = obj_func[obj_func.find('=', c_idx) + 1:]
-    c_parts = re.findall(r'(-?\d+(?:\.\d+)?)\*(x_1|x_2)\^(\d+)', c_str)
-    c = [0, 0]
-    for part in c_parts:
-        coeff = float(part[0])
-        var_idx = int(part[2])
-        if part[1] == 'x_1':
-            c[0] += coeff
-        else:
-            c[1] += coeff
+# 第一个用例
+A1 = np.matrix([[-1, -1]])
+b1 = np.matrix([[-3]])
+c1 = np.matrix([-6, -3])
+x01 = np.array([1, 0])
+tol1 = 0.001
 
-    # 提取约束条件的系数及常数项
-    A_ub, b_ub = [], []
-    for constraint in constraints:
-        constraint = constraint.replace('<=', '>=')
-        constraint = constraint.replace('>=', '<=')
-        constraints_split = constraint.split('=')
-        constraint_vars = constraints_split[0].strip().split(' ')
+# 第二个用例：
+A2 = np.matrix([[-1, -1]])
+b2 = np.matrix([[-2]])
+c2 = np.matrix([-2, 0])
+x02 = np.array([3, 0])
+tol2 = 0.001
 
-        constraint_coefficients = []
-        for var in constraint_vars:
-            if var[-3:] == 'x_1':
-                var_idx = 0
-            else:
-                var_idx = 1
-            coeff_parts = re.findall(r'(-?\d+(?:\.\d+)?)\*{}(\^\d+)?'.format(var[:-3]), var)
-            coeff = float(coeff_parts[0][0])
-            constraint_coefficients.append(coeff)
+# 第三个用例：
+A3 = np.matrix([[-1, -1]])
+b3 = np.matrix([[-1]])
+c3 = np.matrix([0, 0])
+x03 = np.array([1, 0])
+tol3 = 0.001
 
-        constraint_coefficients = [float(constraints_split[1])] + constraint_coefficients
-        A_ub.append(constraint_coefficients[1:])
-        b_ub.append(-constraint_coefficients[0])
+# 第四个用例：
+A4 = np.matrix([[1, 2]])
+b4 = np.matrix([[4]])
+c4 = np.matrix([-4, -4])
+x04 = np.array([1, 0])
+tol4 = 0.001
 
-    # 设置变量边界
-    bounds = [(0, None), (0, None)]
 
-    # 执行线性规划
-    result = optimize.linprog(c, A_ub=A_ub, b_ub=b_ub, bounds=bounds, method='highs', tol=tolerance, options={'maxiter': 1000})
-    results.append(result)
+# 第五个用例：
+A5 = np.matrix([[-1, -1], [1, -2]])
+b5 = np.matrix([[-100], [0]])
+c5 = np.matrix([-98, -277])
+x05 = np.array([0, 1])
+tol5 = 0.001
+
+# result5 = interior_point_method(A5, b5, c5, x05)
+# if result5 is not None:
+#     print("第五个用例的最优解:", result5)
